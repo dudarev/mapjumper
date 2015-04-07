7bc7151dfbf007648d5918a62bdbaf54244779f0
Tomas Kafka <tk@tomaskafka.com>
2015-04-07 18:34:12 +0200

    Completely rewritten to have all map providers at one place - this should make adding new ones much easier.

--- src/mapjumper_content_script.js
+++ src/mapjumper_content_script.js
@@ -6,162 +6,219 @@ if (window == top) {
     });
 }
 
+var DEFAULT_ZOOM = 16;
+
+// order of providers determine order in the popup
+// provider must have hostnameMatch and extract function to work as extractor
+// provider must have generateUrlTemplate and name to act as target
+var mapProviders = [
+	{
+		name: 'Bing Maps',
+		hostnameMatch: /www.bing.com/,
+		extract: function(document) {
+			var link = document.getElementById("MapControl_MapControl");
+			var link_value = link.getAttribute('value');
+			var latLon = link_value.match(/'C':{'Latitude':([\d.-]+),'Longitude':([\d.-]+)/);
+			if (!latLon)
+				return null;
+			return {
+				lat: parseFloat(latLon[1]),
+				lon: parseFloat(latLon[2]),
+				zoom: parseInt(link_value.match(/'Z':(\d+)/)[1], 10)
+			}
+		},
+		generateUrlTemplate: 'http://www.bing.com/maps/?cp=LAT~LON&lvl=ZOOM'
+	},
+	{
+		name: 'Google Maps', // maps.google domain
+		hostnameMatch: /maps.google/,
+		extract: function(document) {
+			var gmap_link = document.getElementById("link");
+			var gmap_link_href = gmap_link.getAttribute('href');
+			var latLon = gmap_link_href.match(/[^s]ll=([\d.-]+),([\d.-]+)/);
+			if (!latLon)
+				return null;
+			return {
+				zoom: parseInt(gmap_link_href.match(/z=(\d*)/)[1], 10),
+				lat: parseFloat(latLon[1]),
+				lon: parseFloat(latLon[2])
+			};
+		},
+		coordinatesNotFound: "no coordinates detected, drag the map around, click again",
+		generateUrlTemplate: 'http://maps.google.com/?ie=UTF8&ll=LAT,LON&z=ZOOM'
+	},
+	{
+		name: 'Google Maps', // google.com/maps domain
+		hostnameMatch: /www.google/,
+		extract: function(document) {
+			var pathname = window.location.pathname;
+			if (pathname) {
+				var latLonZoom = pathname.match(/@([\d.-]+),([\d.-]+),([\d]+)z/);
+				if (latLonZoom) {
+					return {
+						lat: parseFloat(latLonZoom[1]),
+						lon: parseFloat(latLonZoom[2]),
+						zoom: parseFloat(latLonZoom[3])
+					}
+				}
+			}
+			return null;
+		}
+	},
+	{
+		name: 'MapQuest Maps',
+		generateUrlTemplate: 'http://open.mapquest.com/?center=LAT,LON&zoom=ZOOM'
+	},
+	{
+		name: 'OpenStreetMap',
+		hostnameMatch: /openstreetmap.org/,
+		extract: function(document) {
+			var hash = window.location.hash;
+			if(hash){
+				var zlatlon = hash.match(/map=([\d]+)\/([\d.-]+)\/([\d.-]+)/);
+				if (!zlatlon)
+					return null;
+				return {
+					lat: parseFloat(zlatlon[2]),
+					lon: parseFloat(zlatlon[3]),
+					zoom: parseFloat(zlatlon[1])
+				};
+			}
+		},
+		generateUrlTemplate: 'http://www.openstreetmap.org/?lon=LON&lat=LAT&zoom=ZOOM&mlat=LAT&mlon=LON'
+	},
+	{
+		name: 'Foursquare',
+		hostnameMatch: /foursquare/,
+		extract: function(document) {
+			var lat, lon;
+			for (var i = 0; i < document.head.childNodes.length; ++i) {
+				var child = document.head.childNodes[i];
+				if(child.nodeName){
+					if (child.nodeName == 'META') {
+						if(child.getAttribute('property') == "playfoursquare:location:latitude")
+							lat = child.getAttribute('content');
+						if(child.getAttribute('property') == "playfoursquare:location:longitude")
+							lon = child.getAttribute('content');
+					}
+				}
+			}
+			return {
+				lat: lat,
+				lon: lon
+			};
+		},
+		coordinatesNotFound: "browse to a place page",
+	},
+	{
+		name: 'Wikimapia',
+		hostnameMatch: /wikimapia/,
+		extract: function(document) {
+			var hash = window.location.hash;
+			if(hash){
+				var latLonZ = hash.match(/lat=([\d.-]+)&lon=([\d.-]+)&z=([\d]+)/);
+				if (!latLonZ)
+					return null;
+				return {
+					lat: parseFloat(latLonZ[1]),
+					lon: parseFloat(latLonZ[2]),
+					zoom: parseFloat(latLonZ[3])
+				};
+			}
+		},
+		generateUrlTemplate: 'http://wikimapia.org/#lat=LAT&lon=LON&z=ZOOM&l=0&m=b'
+	},
+	{
+		name: 'Yandex Maps',
+		hostnameMatch: /maps.yandex/,
+		extract: function(document) {
+			var link = window.location.href;
+			var lonLat = link.match(/ll=([\d.-]+)(?:%2C|,)([\d.-]+)/);
+			if (!lonLat)
+				return null;
+			return {
+				lon: parseFloat(lonLat[1], 10),
+				lat: parseFloat(lonLat[2], 10),
+				zoom: parseInt(link.match(/z=(\d+)/)[1], 10)
+			}
+		},
+		generateUrlTemplate: 'http://maps.yandex.ru/?ll=LON%2CLAT&z=ZOOM'
+	},
+	{
+		name: '2gis',
+		hostnameMatch: /2gis/,
+		extract: function(document) {
+			var pathname = window.location.pathname;
+			if (pathname) {
+				var latlonz = pathname.match(/([\d.-]+)%2C([\d.-]+)\/zoom\/([\d.-]+)/);
+				return {
+					lon: parseFloat(latlonz[1]),
+					lat: parseFloat(latlonz[2]),
+					zoom: parseFloat(latlonz[3])
+				}
+			} else {
+				return null;
+			}
+		}
+	},
+	{
+		name: 'Mapy.cz',
+		hostnameMatch: /mapy.cz/,
+		extract: function(document) {
+			var search = window.location.search;
+			if (search) {
+				var lonMatch = search.match(/x=([\d.-]+)/);
+				var latMatch = search.match(/y=([\d.-]+)/);
+				var zoomMatch = search.match(/z=([\d]+)/);
+				if (!(latMatch && lonMatch && zoomMatch))
+					return null;
+				return {
+					lat: parseFloat(latMatch[1]),
+					lon: parseFloat(lonMatch[1]),
+					zoom: parseFloat(zoomMatch[1])
+				}
+			}
+		},
+		generateUrlTemplate: 'http://mapy.cz?x=LON&y=LAT&z=ZOOM'
+	}
+];
+
+
 // Search latitude and longitude in meta
 // Return null if none is found.
-var findCoordinates= function() {
-    var node = document.head;
-    var lat = null;
-    var lon = null;
-    var zoom = null;
-    var zoom_default = 16;
-    var hostname = window.location.hostname;
-    if (hostname.match(/maps.google/))
-        hostname = 'maps.google.com';
-    if (hostname.match(/www.google/))
-        hostname = 'www.google.com';
-    if (hostname.match(/openstreetmap.org/))
-        hostname = 'openstreetmap.org';
-    if (hostname.match(/foursquare/))
-        hostname = 'foursquare';
-    if (hostname.match(/wikimapia/))
-        hostname = 'wikimapia';
-    if (hostname.match(/www.bing.com/))
-        hostname = 'bing';
-    if (hostname.match(/maps.yandex/))
-        hostname = 'maps.yandex.ru';
-    if (hostname.match(/2gis/))
-        hostname = '2gis';
-    if (hostname.match(/mapy.cz/))
-        hostname = 'mapycz';
-    switch(hostname){
-        case "foursquare":
-            console.log("foursquare");
-            for (var i = 0; i < node.childNodes.length; ++i) {
-                var child = node.childNodes[i];
-                if(child.nodeName){
-                    if (child.nodeName == 'META') {
-                        if(child.getAttribute('property') == "playfoursquare:location:latitude") lat = child.getAttribute('content');
-                        if(child.getAttribute('property') == "playfoursquare:location:longitude") lon = child.getAttribute('content');
-                    }
-                }
-            }
-            break;
-        case "maps.google.com":
-            console.log("maps.google.com");
-            var gmap_link = document.getElementById("link");
-            gmap_link = gmap_link.getAttribute('href');
-            latlon = gmap_link.match(/[^s]ll=([\d.-]+),([\d.-]+)/);
-            if (!latlon)
-                break;
-            lat = parseFloat(latlon[1]);
-            lon = parseFloat(latlon[2]);
-            zoom = parseInt(gmap_link.match(/z=(\d*)/)[1], 10);
-            break;
-        case "www.google.com":
-            console.log("www.google.com");
-            var pathname = window.location.pathname;
-            if (pathname){
-                latlonz = pathname.match(/@([\d.-]+),([\d.-]+),([\d]+)z/);
-                lat = parseFloat(latlonz[1]);
-                lon = parseFloat(latlonz[2]);
-                zoom = parseFloat(latlonz[3]);
-            }
-            break;
-        case "openstreetmap.org":
-            var hash = window.location.hash;
-            if(hash){
-                zlatlon = hash.match(/map=([\d]+)\/([\d.-]+)\/([\d.-]+)/);
-                if (!zlatlon) break;
-                zoom = parseFloat(zlatlon[1]);
-                lat = parseFloat(zlatlon[2]);
-                lon = parseFloat(zlatlon[3]);
-            }
-            break;
-        case "bing":
-            console.log("bing");
-            var link = document.getElementById("MapControl_MapControl");
-            link = link.getAttribute('value');
-            latlon = link.match(/'C':{'Latitude':([\d.-]+),'Longitude':([\d.-]+)/);
-            if (!latlon)
-                break;
-            var lat = parseFloat(latlon[1]);
-            var lon = parseFloat(latlon[2]);
-            var zoom = parseInt(link.match(/'Z':(\d+)/)[1], 10);
-            break;
-        case "wikimapia":
-            console.log("wikimapia");
-            var hash = window.location.hash;
-            if(hash){
-                console.log(hash);
-                latlonz = hash.match(/lat=([\d.-]+)&lon=([\d.-]+)&z=([\d]+)/);
-                if (!latlonz) break;
-                lat = parseFloat(latlonz[1]);
-                lon = parseFloat(latlonz[2]);
-                zoom = parseFloat(latlonz[3]);
-            }
-            break;
-        case "maps.yandex.ru":
-            console.log("yandex");
-            var link = window.location.href;
-            var lonlat = link.match(/ll=([\d.-]+)(?:%2C|,)([\d.-]+)/);
-            if (!lonlat)
-                break;
-            var lon = parseFloat(lonlat[1], 10);
-            var lat = parseFloat(lonlat[2], 10);
-            var zoom = parseInt(link.match(/z=(\d+)/)[1], 10);
-            break;
-        case "2gis":
-            console.log("2gis");
-            var pathname = window.location.pathname;
-            if (pathname){
-                latlonz = pathname.match(/([\d.-]+)%2C([\d.-]+)\/zoom\/([\d.-]+)/);
-                lon = parseFloat(latlonz[1]);
-                lat = parseFloat(latlonz[2]);
-                zoom = parseFloat(latlonz[3]);
-            }
-        break;
-        case "mapycz":
-            console.log("mapycz");
-            var search = window.location.search;
-            if (search){
-                console.log(search);
-                var lonMatch = search.match(/x=([\d.-]+)/);
-                var latMatch = search.match(/y=([\d.-]+)/);
-                var zoomMatch = search.match(/z=([\d]+)/);
-                if (!(latMatch && lonMatch && zoomMatch)) break;
-                lat = parseFloat(latMatch[1]);
-                lon = parseFloat(lonMatch[1]);
-                zoom = parseFloat(zoomMatch[1]);
-            }
-            break;
-    }
-    if(!(lat === null) && !(lon === null)){
-        return {"lat":lat, "lon":lon, "zoom":zoom};
-    }
-    console.log("no place detected");
-    var not_found_message;
-    switch(hostname){
-        case "foursquare":
-            not_found_message = "browse to a place page";
-            break;
-        case "maps.google.com":
-            not_found_message = "no coordinates detected, drag the map around, click again";
-            break;
-        case "openstreetmap.org":
-            not_found_message = "no coordinates detected";
-            break;
-        case "maps.yandex.ru":
-            not_found_message = "no coordinates detected";
-            break;
-        default:
-            not_found_message = "no coordinates detected";
-    }
-    return {"found": false, "message": not_found_message};
+var findCoordinates = function() {
+	var matchingMapProviders = mapProviders.filter(function(provider){
+		return provider.hostnameMatch && window.location.hostname.match(provider.hostnameMatch);
+	});
+
+	if (matchingMapProviders.length == 0) {
+		return { error: 'Could not find provider for url ' + window.location.hostname + ', please file an issue at https://github.com/dudarev/mapjumper/issues' };
+	}
+	console.log('matching map providers', matchingMapProviders);
+
+	var mapProvider = matchingMapProviders[0];
+	var latLonZoom = mapProvider.extract && mapProvider.extract(document) || null;
+
+	console.log('latLonZoom', latLonZoom);
+
+	if (latLonZoom == null || latLonZoom.lat == null || latLonZoom.lon == null) {
+		var message = mapProvider.coordinatesNotFound || 'No place detected.';
+		console.log('no coordinates found:', message);
+		return { error: message };
+	}
+
+	if (latLonZoom.zoom == null)
+		latLonZoom.zoom = DEFAULT_ZOOM; // default zoom
+
+	return {
+		latLonZoom: latLonZoom,
+		mapProviders: mapProviders
+	};
 };
 
 function initialize(tabId) {
-    chrome.tabs.sendRequest(tabId, {}, function(coordinates_pair) {
-        if (!coordinates_pair) {
+    chrome.tabs.sendRequest(tabId, {}, function(result) {
+        if (!result) {
             chrome.pageAction.hide(tabId);
         } else {
             chrome.pageAction.show(tabId);
--- src/popup.js
+++ src/popup.js
@@ -1,54 +1,28 @@
 function generate_links() {
-    var zoom_default = 16;
-    var coordinates = null;
     chrome.tabs.getSelected(null, function(tab) {
-        chrome.tabs.sendRequest(tab.id, {}, function(coordinates_pair) {
-            coordinates = coordinates_pair;
-            if (coordinates.found === false){
-                document.getElementById('container').innerHTML = coordinates.message;
-            } else {
-                var zoom;
-                if (coordinates.zoom)
-                    zoom = coordinates.zoom;
-                else
-                    zoom = zoom_default;
-                var link_wikimapia = '<a href="http://wikimapia.org/#lat=LAT&lon=LON&z=ZOOM&l=0&m=b" target="_blank">Wikimapia</a>';
-                var link_osm = '<a href="http://www.openstreetmap.org/?lon=LON&lat=LAT&zoom=ZOOM&mlat=LAT&mlon=LON" target="_blank">OpenStreetMap</a>';
-                var link_google ='<a href="http://maps.google.com/?ie=UTF8&ll=LAT,LON&z=ZOOM" target="_blank">Google Maps</a>';
-                var link_mapquest = '<a href="http://open.mapquest.com/?center=LAT,LON&zoom=ZOOM" target="_blank">MapQuest Open</a>';
-                var link_yandex = '<a href="http://maps.yandex.ru/?ll=LON%2CLAT&z=ZOOM" target="_blank">Yandex Maps</a>';
-                var link_bing = '<a href="http://www.bing.com/maps/?cp=LAT~LON&lvl=ZOOM" target="_blank">Bing Maps</a>';
-                var link_mapycz = '<a href="http://mapy.cz?x=LON&y=LAT&z=ZOOM" target="_blank">Mapy.cz</a>';
-                link_wikimapia = link_wikimapia.replace('LAT',coordinates.lat);
-                link_wikimapia = link_wikimapia.replace('LON',coordinates.lon);
-                link_wikimapia = link_wikimapia.replace('ZOOM',zoom);
-                link_osm = link_osm.replace(/LAT/g,coordinates.lat);
-                link_osm = link_osm.replace(/LON/g,coordinates.lon);
-                link_osm = link_osm.replace('ZOOM',zoom);
-                link_google = link_google.replace(/LAT/g,coordinates.lat);
-                link_google = link_google.replace(/LON/g,coordinates.lon);
-                link_google = link_google.replace('ZOOM',zoom);
-                link_mapquest = link_mapquest.replace(/LAT/g,coordinates.lat);
-                link_mapquest = link_mapquest.replace(/LON/g,coordinates.lon);
-                link_mapquest = link_mapquest.replace('ZOOM',zoom);
-                link_yandex = link_yandex.replace(/LAT/g,coordinates.lat);
-                link_yandex = link_yandex.replace(/LON/g,coordinates.lon);
-                link_yandex = link_yandex.replace('ZOOM',zoom);
-                link_bing = link_bing.replace(/LAT/g,coordinates.lat);
-                link_bing = link_bing.replace(/LON/g,coordinates.lon);
-                link_bing = link_bing.replace('ZOOM',zoom);
-                link_mapycz = link_mapycz.replace(/LAT/g,coordinates.lat);
-                link_mapycz = link_mapycz.replace(/LON/g,coordinates.lon);
-                link_mapycz = link_mapycz.replace('ZOOM',zoom);
-                document.getElementById('container').innerHTML =
-                    link_bing + "<br/>" +
-                    link_google + "<br/>" +
-                    link_mapquest + "<br/>" +
-                    link_osm + "<br/>" +
-                    link_wikimapia + "<br/>" +
-                    link_yandex + "<br/>" +
-                    link_mapycz;
-            }
+        chrome.tabs.sendRequest(tab.id, {}, function(result) {
+					console.log(result);
+					if (result.error) {
+						document.getElementById('container').innerHTML = result.error;
+					} else {
+						var links = result.mapProviders.reduce(function(array, mapProvider) {
+							if (mapProvider.generateUrlTemplate) {
+								var mapUrl = mapProvider.generateUrlTemplate
+									.replace(/LAT/g, result.latLonZoom.lat)
+									.replace(/LON/g, result.latLonZoom.lon)
+									.replace('ZOOM', result.latLonZoom.zoom);
+
+								var url = '<a href="URL" target="_blank">NAME</a>';
+								array.push(url
+									.replace('NAME', mapProvider.name)
+									.replace('URL', mapUrl)
+								);
+							}
+							return array;
+						}, []);
+
+						document.getElementById('container').innerHTML = links.join('<br/>');
+					}
         });
     });
 }
